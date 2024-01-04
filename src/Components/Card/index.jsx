a8515bc643bef489e6005bcd2214e4d8cf6db045
brandonghostbull044 <brandonlglz000@gmail.com>
2024-01-04 01:38:29 -0600

    04-01-24

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -2,13 +2,13 @@ import { useContext } from "react";
 import { GlobalContext } from "../../Context";
 
 function Card (props) {
-    const { addToCart, setOpenDetail, openDetail } = useContext(GlobalContext);
+    const { addToCart, openDetail, setOpenDetail } = useContext(GlobalContext);
 
     return (
-        <div className="bg-white cursor-pointer w-56 h-60">
+        <div className="bg-white cursor-pointer w-56 h-60 mx-auto">
             <figure className="relative mb-2 w-full h-4/5">
                 <span className="absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5">{props.category}</span>
-                <img className="touchShow w-full object-cover rounded-lg" src={props.image[0]}/>
+                <img id={props.id} className="touchShow w-full object-cover rounded-lg" src={props.image[0]}  onClick={() => {setOpenDetail(!openDetail)}}/>
                 <div className="absolute top-0 right-0 flex justify-center bg-white w-6 h6 rounded-full m-2 p-1" onClick={addToCart}>+</div>
             </figure>
             <p className="flex justify-between">
--- src/Components/Portals/ProductDetailContainer/index.jsx
+++ src/Components/Portals/ProductDetailContainer/index.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import { createPortal } from "react-dom";
 
-function ProductDetailContainer({children}) {
+function ProductDetailContainer() {
     return createPortal(
-        <div className="noTouchShow w-80 h-80 sticky bg-white bottom-full rounded-lg border-2 border-black">
-            {children}
+        <div className="noTouchShow w-full relative flex flex-col items-center h-4/5 sticky bg-white bottom-full rounded-lg border-2 border-black mt-32">
+            <div className="flex flex-col  items-center w-full h-4/5">
+                <h3 className="text-center text-4xl absolute">Pantalon</h3>
+                <img src="https://i.imgur.com/ZKGofuB.jpeg" alt="" className="w-full h-2/3"/>
+                <span className="w-fit text-2xl">Ropa</span>
+                <p className="w-fit text-lg">Pantalon roto</p>
+            </div>
+            <button className="border-black rounded-3xl border w-2/5 mt-auto mb-10">Agregar</button>
         </div>,
         document.getElementById('productDetailContainer')
     );
