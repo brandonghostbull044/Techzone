ebb2b915a74207e0e2f2e9cbe8c146ab2f613e9f
brandonlglzArch <brandonleongonzalezdev@gmail.com>
2024-01-26 13:20:57 -0600

    26-01-24 Agregando funcionalidad de detail card

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -2,16 +2,6 @@ import { useContext } from "react";
 import { GlobalContext } from "../../Context";
 
 function Card (props) {
-<<<<<<< HEAD
-    const { addToCart, openDetail, setOpenDetail } = useContext(GlobalContext);
-
-    return (
-        <div className="bg-white cursor-pointer w-56 h-60 mx-auto">
-            <figure className="relative mb-2 w-full h-4/5">
-                <span className="absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5">{props.category}</span>
-                <img id={props.id} className="touchShow w-full object-cover rounded-lg" src={props.image[0]}  onClick={() => {setOpenDetail(!openDetail)}}/>
-                <div className="absolute top-0 right-0 flex justify-center bg-white w-6 h6 rounded-full m-2 p-1" onClick={addToCart}>+</div>
-=======
     const { addToCart, openCartDetail } = useContext(GlobalContext);
 
     return (
@@ -19,7 +9,6 @@ function Card (props) {
             <figure className="noTouch cursor-pointer relative w-full h-5/6 bg-center bg-cover rounded-xl" style={{backgroundImage: `url(${props.image[0]})`}} onClick={(e) => {openCartDetail([props.image[0], props.title, props.category, props.price, 'Descripcion Descripcion Descripcion Descripcion Descripcion Descripcion Descripcion'], e.target)}}>
                 <span className="noActivate noTouch absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5">{props.category}</span>
                 <div className="noActivate noTouch absolute top-0 right-0 flex justify-center bg-white w-6 h6 rounded-full m-2 p-1 cursor-pointer" onClick={addToCart}>+</div>
->>>>>>> 8d868b7 (26-01-24)
             </figure>
             <p className="noTouch flex justify-between items-center h-1/6">
                 <span className="noTouch text-sm h-fit text-center font-bold relative z-10 w-5/6">{props.title}</span>
--- src/Components/Portals/ProductDetailContainer/index.jsx
+++ src/Components/Portals/ProductDetailContainer/index.jsx
@@ -1,21 +1,10 @@
 import React from "react";
 import { createPortal } from "react-dom";
 
-function ProductDetailContainer() {
+function ProductDetailContainer({children}) {
     return createPortal(
-<<<<<<< HEAD
-        <div className="noTouchShow w-full relative flex flex-col items-center h-4/5 sticky bg-white bottom-full rounded-lg border-2 border-black mt-32">
-            <div className="flex flex-col  items-center w-full h-4/5">
-                <h3 className="text-center text-4xl absolute">Pantalon</h3>
-                <img src="https://i.imgur.com/ZKGofuB.jpeg" alt="" className="w-full h-2/3"/>
-                <span className="w-fit text-2xl">Ropa</span>
-                <p className="w-fit text-lg">Pantalon roto</p>
-            </div>
-            <button className="border-black rounded-3xl border w-2/5 mt-auto mb-10">Agregar</button>
-=======
         <div className="w-full h-full bg-white bottom-full rounded-3xl border-2 border-black bg-opacity-95">
             {children}
->>>>>>> 8d868b7 (26-01-24)
         </div>,
         document.getElementById('productDetailContainer')
     );
